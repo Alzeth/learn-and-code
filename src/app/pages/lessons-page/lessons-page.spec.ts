7a8a7bd2236d3577813a3d16ece0d5e7bd7e8685
Igor Ovcharenko <igor_ovcharenko@epam.com>
2026-04-04 19:33:35 +0300

    1.0.0 Test fix

--- src/app/pages/lessons-page/lessons-page.spec.ts
+++ src/app/pages/lessons-page/lessons-page.spec.ts
@@ -5,18 +5,34 @@ import { LessonsPage } from './lessons-page';
 
 describe('LessonsPage', () => {
   const fakeActivatedRoute = {
-    snapshot: { data: { } }
-  } as ActivatedRoute;
+    snapshot: {data: {lessons: {lessons: []}}}
+  } as unknown as ActivatedRoute;
 
   let component: LessonsPage;
   let fixture: ComponentFixture<LessonsPage>;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {},
+      }),
+    });
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LessonsPage],
       providers: [{provide: ActivatedRoute, useValue: fakeActivatedRoute}],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LessonsPage);
     component = fixture.componentInstance;
--- src/app/services/lessons.spec.ts
+++ src/app/services/lessons.spec.ts
@@ -5,6 +5,22 @@ import { LessonsService } from './lessons';
 describe('Lessons', () => {
   let service: LessonsService;
 
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {},
+      }),
+    });
+  });
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(LessonsService);
