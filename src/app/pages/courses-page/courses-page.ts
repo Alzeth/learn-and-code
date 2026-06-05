e2b3103f3eadd12b88fef9202b23e03506c0f26c
Igor Ovcharenko <mr.alzeth@gmail.com>
2026-06-05 19:34:37 +0300

    1.1.0 Added Courses fetch service (#60)
    * 1.1.0 Added Courses fetch service
    
    * 1.1.0 Fixed tests
    
    ---------
    
    Co-authored-by: Igor Ovcharenko <mr.alzeth2gmail.com>

--- src/app/pages/courses-page/courses-page.ts
+++ src/app/pages/courses-page/courses-page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ICourse } from '@app/interfaces';
+import { LessonsList } from 'app/components/lessons/list/list';
+import { Item } from 'app/components/lessons/item/item';
 
 @Component({
   selector: 'app-courses-page',
@@ -7,5 +12,12 @@ import { Component } from '@angular/core';
   styleUrl: './courses-page.css',
 })
 export class CoursesPage {
+  courses: ICourse[] = [];
 
+  private route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    const response = this.route.snapshot.data["courses"];
+    this.courses = response.courses;
+  }
 }
---
+++ src/app/resolvers/courses.resolver.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { CoursesService } from '@app/services/courses';
+import { ICoursesResponse } from '@app/services/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesResolver implements Resolve<ICoursesResponse> {
+  private coursesService: CoursesService = inject(CoursesService);
+
+  resolve(): Observable<ICoursesResponse> {
+    return this.coursesService.getAll();
+  }
+}
