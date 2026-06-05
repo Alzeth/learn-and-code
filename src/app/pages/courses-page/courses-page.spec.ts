af62b609c497aa175140e426c89fb7440a0baed2
Igor Ovcharenko <mr.alzeth2gmail.com>
2026-06-05 19:33:26 +0300

    1.1.0 Fixed tests

--- src/app/pages/courses-page/courses-page.spec.ts
+++ src/app/pages/courses-page/courses-page.spec.ts
@@ -1,14 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoursesPage } from './courses-page';
+import { ActivatedRoute } from '@angular/router';
 
 describe('CoursesPage', () => {
+  const fakeActivatedRoute = {
+    snapshot: {data: {courses: {courses: []}}}
+  } as unknown as ActivatedRoute;
+
   let component: CoursesPage;
   let fixture: ComponentFixture<CoursesPage>;
 
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {},
+      }),
+    });
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CoursesPage]
+      imports: [CoursesPage],
+      providers: [{ provide: ActivatedRoute, useValue: fakeActivatedRoute }]
     })
     .compileComponents();
 
