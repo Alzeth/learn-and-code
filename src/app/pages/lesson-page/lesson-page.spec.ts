3adb958f322f266fecdd47fa2f8cd57df935225b
Igor Ovcharenko <mr.alzeth2gmail.com>
2026-06-09 21:47:05 +0300

    2.0.0 Test fix

--- src/app/pages/lesson-page/lesson-page.spec.ts
+++ src/app/pages/lesson-page/lesson-page.spec.ts
@@ -2,17 +2,44 @@ import { importProvidersFrom } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
+import { provideMarkdown } from 'ngx-markdown';
+import { provideHttpClient } from '@angular/common/http';
 
 import { LessonPage } from './lesson-page';
 
 describe('LessonPage', () => {
+  const fakeActivatedRoute = {
+    snapshot: {data: {lesson: '', theory: ''}}
+  } as unknown as ActivatedRoute;
+
   let component: LessonPage;
   let fixture: ComponentFixture<LessonPage>;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {},
+      }),
+    });
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LessonPage],
-      providers: [importProvidersFrom(MonacoEditorModule.forRoot())]
+      providers: [
+        importProvidersFrom(MonacoEditorModule.forRoot()),
+        provideHttpClient(),
+        provideMarkdown(),
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ]
     })
     .compileComponents();
 
