40c9165bf60d8d173ca27e65fbd54c7d2bca9190
Igor Ovcharenko <mr.alzeth2gmail.com>
2026-06-15 15:19:40 +0300

    6.0.1 Env config fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
 interface ImportMeta {
-  readonly env: Record<string, string | undefined>;
+  readonly env: {
+    readonly NG_APP_API_URL?: string;
+    readonly NG_APP_USE_LOCAL_DATA?: string;
+    readonly [key: string]: string | undefined;
+  };
 }
