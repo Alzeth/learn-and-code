3d53763adb6ef3d4493abfe3acea671999592e69
Igor Ovcharenko <mr.alzeth2gmail.com>
2026-06-15 16:10:23 +0300

    6.1.0 Update server config. Moving routes to client side rendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,7 +4,15 @@ import { ROUTES } from 'app/constants';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ROUTES.LESSONS,
+    renderMode: RenderMode.Client,
+  },
+  {
     path: ROUTES.LESSON,
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: ROUTES.COURSES,
     renderMode: RenderMode.Client,
   },
   {
