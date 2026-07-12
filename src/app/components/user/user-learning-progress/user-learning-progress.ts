aefea5bc68275ce9b63b8de4ec63e6a67dc73f03
Igor Ovcharenko <mr.alzeth@gmail.com>
2026-07-12 03:31:47 +0300

    11.2.0 User components, small fixes, shared components (#125)
    Co-authored-by: Igor Ovcharenko <mr.alzeth2gmail.com>

---
+++ src/app/components/user/user-learning-progress/user-learning-progress.ts
@@ -0,0 +1,22 @@
+import { Component, input } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideBookSearch, lucideScrollText } from '@ng-icons/lucide';
+
+import { ZardProgressBarComponent } from 'app/shared/components/progress-bar';
+
+@Component({
+  selector: 'app-user-learning-progress',
+  imports: [
+    NgIcon,
+    ZardProgressBarComponent,
+  ],
+  viewProviders: [provideIcons({ lucideBookSearch, lucideScrollText })],
+  templateUrl: './user-learning-progress.html',
+  styleUrl: './user-learning-progress.css',
+})
+export class UserLearningProgress {
+  readonly completedCourses = input<number>(0);
+  readonly totalCourses = input<number>(0);
+  readonly completedLessons = input<number>(0);
+
+}
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,2 @@
+export const LOGO_PATH_PRD = 'https://alzeth.github.io/learn-and-code/logo.png';
+export const LOGO_NO_TEXT_PATH_PRD = 'https://alzeth.github.io/learn-and-code/logo-no-text.png';
