cfd0404e3055105ed0f514d0a39a273d6cb7aa69
Igor Ovcharenko <mr.alzeth2gmail.com>
2026-07-16 04:26:35 +0300

    13.0.0 Lint fixes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,8 +16,8 @@ import { provideMarkdown } from 'ngx-markdown';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 import { authInterceptor } from 'app/services/auth.interceptor';
-import { localeInterceptor } from 'app/services/locale.interceptor';
 import { AuthService } from 'app/services/auth/auth.service';
+import { localeInterceptor } from 'app/services/locale.interceptor';
 import { TranslocoBrowserLoader } from 'app/services/transloco-browser.loader';
 import { provideZard } from 'app/shared/core/provider/providezard';
 
